56983a998344895f2eb81a22afff7f16f147b280
sjwhole <bluemoon2718@naver.com>
2021-12-19 18:07:56 +0900

    feat: Add lecture router

---
+++ src/routes/lectures.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import {
+  getFullSoonLecture,
+  getLectureInfo,
+  getLimitLecture,
+} from "../utils/lecture";
+
+const router = express.Router();
+
+router.get("", async (req, res) => {
+  const search: string = req.query.search as string;
+  let lectures = await getLectureInfo(search);
+  lectures = lectures.map(
+    (e: {
+      haksuNo: string;
+      suupNo: string;
+      gwamokNm: string;
+      hakjeom: number;
+      daepyoGangsaNm: string;
+      suupTimes: string;
+      jehanInwon: string;
+    }) => {
+      return {
+        haksuNo: e.haksuNo,
+        suupNo: e.suupNo,
+        gwamokNm: e.gwamokNm,
+        daepyoGangsaNm: e.daepyoGangsaNm,
+        hakjeom: e.hakjeom,
+        suupTimes: e.suupTimes,
+        jehanInwon: e.jehanInwon,
+      };
+    }
+  );
+  res.json([lectures]);
+});
+
+router.get("/limits", async (req, res) => {
+  const limitLectures = await getLimitLecture();
+  res.json(limitLectures);
+});
+
+router.get("/soon", async (req, res) => {
+  const fullSoonLectures = await getFullSoonLecture();
+  res.json(fullSoonLectures);
+});
+
+export default router;
