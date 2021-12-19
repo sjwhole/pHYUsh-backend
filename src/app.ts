9a3c4633dc6f3cd57708e745f18e1559ba253d0f
sjwhole <bluemoon2718@naver.com>
2021-12-19 16:00:25 +0900

    feat: Add lecture router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,38 @@
 import express from "express";
+import { sequelize } from "./models";
+import { getSugangInfo } from "./utils/lecture";
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "hello" });
+router.get("/lectures", async (req, res) => {
+  const search: string = req.query.search as string;
+  let lectures = await getSugangInfo(search);
+  lectures = lectures.map(
+    (e: {
+      haksuNo: string;
+      suupNo: string;
+      gwamokNm: string;
+      hakjeom: number;
+      daepyoGangsaNm: string;
+      suupTimes: string;
+      jehanInwon: string;
+    }) => {
+      return {
+        haksuNo: e.haksuNo,
+        suupNo: e.suupNo,
+        gwamokNm: e.gwamokNm,
+        daepyoGangsaNm: e.daepyoGangsaNm,
+        hakjeom: e.hakjeom,
+        suupTimes: e.suupTimes,
+        jehanInwon: e.jehanInwon,
+      };
+    }
+  );
+  res.json([lectures]);
 });
 
 app.use("", router);
-app.listen(3000);
+app.listen(3000, async () => {
+  console.log(`Server Listening on localhost:3000`);
+});
