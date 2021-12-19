8bafe4621c845e21439b793ac8384aeed88b7d3b
sjwhole <bluemoon2718@naver.com>
2021-12-19 18:08:16 +0900

    feat: Seperate router

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,12 @@
 import express from "express";
-import { sequelize } from "./models";
-import { getSugangInfo } from "./utils/lecture";
+import LecutreRouter from "./routes/lectures";
 
 const app = express();
 const router = express.Router();
 
-router.get("/lectures", async (req, res) => {
-  const search: string = req.query.search as string;
-  let lectures = await getSugangInfo(search);
-  lectures = lectures.map(
-    (e: {
-      haksuNo: string;
-      suupNo: string;
-      gwamokNm: string;
-      hakjeom: number;
-      daepyoGangsaNm: string;
-      suupTimes: string;
-      jehanInwon: string;
-    }) => {
-      return {
-        haksuNo: e.haksuNo,
-        suupNo: e.suupNo,
-        gwamokNm: e.gwamokNm,
-        daepyoGangsaNm: e.daepyoGangsaNm,
-        hakjeom: e.hakjeom,
-        suupTimes: e.suupTimes,
-        jehanInwon: e.jehanInwon,
-      };
-    }
-  );
-  res.json([lectures]);
-});
+app.use("/lectures", LecutreRouter);
 
 app.use("", router);
 app.listen(3000, async () => {
-  console.log(`Server Listening on localhost:3000`);
+  console.log(`Server Listening on http://127.0.0.1:3000`);
 });
