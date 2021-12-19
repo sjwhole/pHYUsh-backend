70e80ccbe84ed62b2839e419eb9e2dd4fffeb645
sjwhole <bluemoon2718@naver.com>
2021-12-19 22:00:18 +0900

    refactor: Remove unused router

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,9 @@ import express from "express";
 import LecutreRouter from "./routes/lecture";
 
 const app = express();
-const router = express.Router();
 
 app.use("/lectures", LecutreRouter);
 
-app.use("", router);
 app.listen(3000, async () => {
   console.log(`Server Listening on http://127.0.0.1:3000`);
 });
