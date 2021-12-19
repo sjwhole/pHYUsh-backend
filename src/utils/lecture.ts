10fcace7127b6bc8b7201aa79c1b20eb3742938f
sjwhole <bluemoon2718@naver.com>
2021-12-19 18:07:37 +0900

    feat: Add lecture utils

--- src/utils/lecture.ts
+++ src/utils/lecture.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import e from "express";
+import client from "../client";
 
-export const getSugangInfo = async (lectureName: string) => {
+export const getLectureInfo = async (lectureName: string) => {
   let response = await axios.post(
     "https://portal.hanyang.ac.kr/sugang/SgscAct/findSuupSearchSugangSiganpyo.do?pgmId=P310278&menuId=M006631&tk=e9068598524e004a4af6d96d34410fa56705e76bb2f7fc2df35729471b0f3b45",
     {
@@ -36,4 +38,68 @@ export const getSugangInfo = async (lectureName: string) => {
   let data = response.data;
   let lectures = data.DS_SUUPGS03TTM01[0].list;
   return lectures;
+};
+
+export const updateLectureInfo = async () => {
+  const lectures = await getLectureInfo("");
+  lectures.map(
+    async (e: {
+      haksuNo: string;
+      suupNo: string;
+      gwamokNm: string;
+      hakjeom: number;
+      daepyoGangsaNm: string;
+      suupTimes: string;
+      jehanInwon: string;
+    }) => {
+      const {
+        haksuNo,
+        suupNo,
+        gwamokNm,
+        hakjeom,
+        daepyoGangsaNm,
+        suupTimes,
+        jehanInwon,
+      } = e;
+      const inwonInfo = jehanInwon.split("/");
+      const currentInwon = parseInt(inwonInfo[0]);
+      const limitInwon = parseInt(inwonInfo[1]);
+
+      await client.lecture.upsert({
+        create: {
+          suupNo,
+          haksuNo,
+          gwamokNm,
+          hakjeom,
+          daepyoGangsaNm,
+          suupTimes,
+          currentInwon,
+          limitInwon,
+        },
+        update: {
+          haksuNo,
+          gwamokNm,
+          hakjeom,
+          daepyoGangsaNm,
+          suupTimes,
+          currentInwon,
+          limitInwon,
+        },
+        where: {
+          suupNo,
+        },
+      });
+    }
+  );
+};
+
+export const getLimitLecture = async () => {
+  return client.$queryRaw`SELECT * FROM "Lecture" WHERE "limitInwon" <= "currentInwon"`;
+};
+
+export const getFullSoonLecture = async () => {
+  return client.$queryRaw`SELECT *
+  FROM "Lecture"
+  WHERE "limitInwon" > "currentInwon"
+  ORDER BY "limitInwon" - "currentInwon"`;
 };
