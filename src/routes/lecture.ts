be8d4660214f368d05c8ba2b01c87b0e68b411bc
Seongjun Kim <66054861+sjwhole@users.noreply.github.com>
2021-12-21 21:42:50 +0900

    Feature/lecture search (#3)
    * feat: Add search lecture
    
    * feat: Change lecture get by sarchLecture

--- src/routes/lecture.ts
+++ src/routes/lecture.ts
@@ -1,40 +1,18 @@
 import express from "express";
-import {
-  getFullSoonLecture,
-  getLectureInfo,
-} from "../utils/lecture";
+import { getFullSoonLecture, searchLecture } from "../utils/lecture";
 
 const router = express.Router();
 
 router.get("", async (req, res) => {
   const search: string = req.query.search as string;
-  let lectures = await getLectureInfo(search);
-  lectures = lectures.map(
-    (e: {
-      haksuNo: string;
-      suupNo: string;
-      gwamokNm: string;
-      hakjeom: number;
-      daepyoGangsaNm: string;
-      suupTimes: string;
-      jehanInwon: string;
-    }) => {
-      return {
-        haksuNo: e.haksuNo,
-        suupNo: e.suupNo,
-        gwamokNm: e.gwamokNm,
-        daepyoGangsaNm: e.daepyoGangsaNm,
-        hakjeom: e.hakjeom,
-        suupTimes: e.suupTimes,
-        jehanInwon: e.jehanInwon,
-      };
-    }
-  );
+  let lectures = await searchLecture(search);
+
   res.json([lectures]);
 });
 
 router.get("/soon", async (req, res) => {
   const fullSoonLectures = await getFullSoonLecture();
+
   res.json(fullSoonLectures);
 });
 
