172280c36ab85dccb7478331078d3ddc44336a90
sjwhole <bluemoon2718@naver.com>
2021-12-22 10:36:28 +0900

    feat: Add get push token check

--- src/routes/push.ts
+++ src/routes/push.ts
@@ -5,6 +5,13 @@ const router = express.Router();
 
 router.get("", async (req, res) => {
   const token = req.query.token as string;
+  if (token === undefined) {
+    res.status(400);
+    res.json({
+      message: "URL param 'token' is required.",
+    });
+    return;
+  }
   const query = await getPushInfos(token);
 
   res.json(query?.pushes);
