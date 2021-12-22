a3b370ce3806502d2076efa78e10fe330761d0db
sjwhole <bluemoon2718@naver.com>
2021-12-22 10:24:41 +0900

    feat: Add push router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import LecutreRouter from "./routes/lecture";
+import PushRouter from "./routes/push";
 
 const app = express();
 
 app.use("/lectures", LecutreRouter);
+app.use("/pushes", PushRouter);
 
 app.listen(3000, async () => {
   console.log(`Server Listening on http://127.0.0.1:3000`);
