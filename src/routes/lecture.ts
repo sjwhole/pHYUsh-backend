520bb6c93ad2e100e70db31c9f8068fb92a38d47
sjwhole <bluemoon2718@naver.com>
2022-01-27 23:02:12 +0900

    feat: Add type to routers

--- src/routes/lecture.ts
+++ src/routes/lecture.ts
@@ -1,16 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { getFullSoonLecture, searchLecture } from "../utils/lecture";
 
 const router = express.Router();
 
-router.get("", async (req, res) => {
+router.get("", async (req: Request, res: Response) => {
   const search: string = req.query.search as string;
   let lectures = await searchLecture(search);
 
   res.json(lectures);
 });
 
-router.get("/soon", async (req, res) => {
+router.get("/soon", async (req: Request, res: Response) => {
   const fullSoonLectures = await getFullSoonLecture();
 
   res.json(fullSoonLectures);
