2b6c06e4ac102833fb79f73fb09906746a992e2e
sjwhole <bluemoon2718@naver.com>
2022-02-03 20:01:24 +0900

    feat: Add morgan log common

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import LectureRouter from "./routes/lecture";
 import PushRouter from "./routes/push";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
+app.use(morgan("common"));
 
 app.use("/lectures", LectureRouter);
 app.use("/push", PushRouter);
